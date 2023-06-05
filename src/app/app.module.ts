021dea72a34dad6e102c1bd62c5c05c5b3f7a9b5
LucieneFagundes <lfnascimento99@gmail.com>
2023-06-05 11:08:01 -0300

    :bricks: refact structure by feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,22 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { CoreModule } from './core/core.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    DashboardComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+
+    DashboardModule,
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
